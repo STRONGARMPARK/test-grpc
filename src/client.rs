597b9c9abc4801fea780b51e583bef83d4755adf
Armstrong Park <goarmstrongpark.ap@gmail.com>
2022-12-30 12:34:36 -0500

    receive stream working

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,4 @@
+use futures::stream::iter;
 use chat::chat_client::ChatClient;
 use chat::ChatRequest;
 mod chat {
@@ -11,17 +12,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     let mut client = ChatClient::new(channel);
-    let request = tonic::Request::new(
+    let request = tonic::Request::new(iter(vec![
         ChatRequest {
-            username:String::from("armstrong"),
-            content:String::from("Armstrong's Message Content")
+            username: String::from("Rohan"),
+            content: String::from("Rohan Content")
+        },
+        ChatRequest {
+            username: String::from("Marcos"),
+            content: String::from("Marcos Content")
+        },
+        ChatRequest {
+            username: String::from("Armstrong"),
+            content: String::from("Armstrong Content")
         }
-    );
-    
+    ]));
+    // sending stream
+    let response = client.receive_stream(request).await?.into_inner();
+    println!("Response=\n{:?}", response);
+
+    let request = tonic::Request::new(ChatRequest {
+        username: String::from("armstrong"),
+        content: String::from("Armstrong's Message Content"),
+    });
+
     let mut response = client.send_stream(request).await?.into_inner();
+
     while let Some(res) = response.message().await? {
         println!("NOTE = {:?}", res);
     }
     Ok(())
 }
 
+    // // sending stream
+    //     let response = client.receive_stream(request).await?.into_inner();
+    //     println!("RESPONSE=\n{}", response.message);
+    //     Ok(())
+    // }
--- src/server.rs
+++ src/server.rs
@@ -1,36 +1,52 @@
-use tokio::sync::mpsc;
 use chat::chat_server::{Chat, ChatServer};
 use chat::{ChatRequest, ChatResponse};
+use tokio::sync::mpsc;
 use tonic::{transport::Server, Request, Response, Status};
 mod chat {
     tonic::include_proto!("chat");
 }
-
 
 #[derive(Default)]
 pub struct MyChat {}
 
 #[tonic::async_trait]
 impl Chat for MyChat {
-    type SendStreamStream=mpsc::Receiver<Result<ChatResponse,Status>>;
+    async fn receive_stream(
+        &self,
+        request: Request<tonic::Streaming<ChatRequest>>,
+    ) -> Result<Response<ChatResponse>, Status> {
+        let mut stream = request.into_inner();
+        let mut content = String::from("");
+        while let Some(req) = stream.message().await? {
+            content.push_str(&format!("Hello {}\n", req.username))
+        }
+        Ok(Response::new(ChatResponse {content, username: String::from("Armstrong")}))
+    }
+
+    type SendStreamStream = mpsc::Receiver<Result<ChatResponse, Status>>;
     // Spawn an asynchronous task and then return the receiver
-    async fn send_stream(&self, request: Request<ChatRequest>) -> Result<Response<Self::SendStreamStream>, Status> {
+    async fn send_stream(
+        &self,
+        request: Request<ChatRequest>,
+    ) -> Result<Response<Self::SendStreamStream>, Status> {
         let (mut tx, rx) = mpsc::channel(4);
         tokio::spawn(async move {
-            for i in 0..4{
+            for i in 0..4 {
                 tx.send(Ok(ChatResponse {
-                    username:String::from("armstrong"),
-                    content:format!("Content number: {}", i)
-                })).await;
+                    username: String::from("armstrong"),
+                    content: format!("Content number: {}", i),
+                }))
+                .await;
             }
         });
         Ok(Response::new(rx))
     }
 
+
     async fn send(&self, request: Request<ChatRequest>) -> Result<Response<ChatResponse>, Status> {
         Ok(Response::new(ChatResponse {
             username: format!("hello {}", request.get_ref().username),
-            content: String::from("Insert Content Here")
+            content: String::from("Insert Content Here"),
         }))
     }
 }
@@ -47,4 +63,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     Ok(())
 }
-
